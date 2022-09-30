a84fd6a9621af0ac47f7a69d369a816d94a6abf1
Kristina Nikolova <kristina.nikolova.ext@dwpbank.de>
2022-09-30 18:13:03 +0200

    cleanup

--- src/main/java/com/monitoring/demo/config/SecurityConfiguration.java
+++ src/main/java/com/monitoring/demo/config/SecurityConfiguration.java
@@ -2,7 +2,7 @@ package com.monitoring.demo.config;
 
 import com.monitoring.demo.endpoints.CustomEndpoint;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,9 +15,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
+                .requestMatchers(EndpointRequest.to(MetricsEndpoint.class)).hasRole("admin")
                 .requestMatchers(EndpointRequest.to(CustomEndpoint.class)).hasRole("admin");
-               // .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("admin");
         http.csrf().and().httpBasic();
     }
 }
--- src/main/java/com/monitoring/demo/endpoints/CustomEndpoint.java
+++ src/main/java/com/monitoring/demo/endpoints/CustomEndpoint.java
@@ -1,20 +1,18 @@
 package com.monitoring.demo.endpoints;
 
 import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.function.Supplier;
 
 @Endpoint(id = "customEndpoint")
 @Component
 public class CustomEndpoint {
+
+    private final Counter myCounter;
 
     @ReadOperation
     public String customEndpoint(int id) {
@@ -26,20 +24,12 @@ public class CustomEndpoint {
         return fruitsMap.get(id);
     }
 
-    private final Counter myCounter;
 
     public CustomEndpoint(MeterRegistry registry) {
         myCounter = Counter
                 .builder("mycustomcounter")
                 .description("count url hits")
                 .register(registry);
-        Gauge.builder("list-size",fetchListSize()).register(registry);
-    }
-
-    public Supplier<Number> fetchListSize() {
-        List<String> dummyList = new ArrayList<>();
-        dummyList.add("test");
-        return dummyList::size;
     }
 
 }
